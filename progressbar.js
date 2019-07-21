4df5f9f220e9f7617fc41e1803c1ba8cd8d764e4
ayyappa gollu <50751386+ayyappag10@users.noreply.github.com>
2019-07-21 14:22:10 +0530

    Add files via upload
    added perecent text field to show progress percentage.
    added inner rectangle to make the progress bar look usual.

--- progressbar.js
+++ progressbar.js
@@ -2,16 +2,20 @@
 window.addEventListener('load', function(){
     newFunction();
     rects[1].style.tr
-    setInterval(rotate, 100);
+    setInterval(rotate, 20);
 })
 
 var rects;
 var keyrect;
+var percenttext;
 var rotated = 0;
+
 
 function newFunction() {
     rects = document.getElementsByClassName("foregroundrect");
     keyrect = document.getElementById("keyrect");
+    percenttext = document.getElementById("percent");
+    console.log(percenttext)
 }
 
 function rotate(){
@@ -41,6 +45,8 @@ function rotate(){
         rects[1].style.visibility = "visible";
         rects[1].style.backgroundColor = "#0000ff"
     }    
-    rotated = rotated + 10;
+    rotated++;
+    
+    percenttext.innerText = (Math.round(rotated/3.6)) + "%";
     keyrect.style.transform = `rotate(${rotated}deg)`;
 }
